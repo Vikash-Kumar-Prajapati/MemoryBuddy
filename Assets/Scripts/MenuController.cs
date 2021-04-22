036246c90a281fc2b55b209bc7234b1d04614c06
Vikash Kumar Prajapati <vikkurohit10357@gmail.com>
2021-04-22 14:35:11 +0530

    completed

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuController : MonoBehaviour
+{
+	public GameObject mainMenuPanel;
+	public GameObject playMenuPanel;
+
+	public GameObject easyTab;
+	public GameObject normalTab;
+	public GameObject hardTab;
+
+	public Color activeColor;
+	public Color inactiveColor;
+
+
+
+	void Awake()
+	{
+		playMenuPanel.SetActive(true);
+	}
+
+
+
+	public void PlayGame(int difficulty)
+	{
+		PlayerPrefs.SetInt("Difficulty", difficulty);
+
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	
+	void ChangeTabLabel(Difficulty difficulty)
+	{
+		easyTab.GetComponent<Image>().color = inactiveColor;
+		normalTab.GetComponent<Image>().color = inactiveColor;
+		hardTab.GetComponent<Image>().color = inactiveColor;
+
+		switch (difficulty)
+		{
+			case Difficulty.EASY:
+				easyTab.GetComponent<Image>().color = activeColor;
+				break;
+			case Difficulty.NORMAL:
+				normalTab.GetComponent<Image>().color = activeColor;
+				break;
+			case Difficulty.HARD:
+				hardTab.GetComponent<Image>().color = activeColor;
+				break;
+			default:
+				normalTab.GetComponent<Image>().color = activeColor;
+				break;
+		}
+	}
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+	public TMP_Text countText;
+
+	int movesCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+		countText.text = "0";
+    }
+
+
+	public void ChangeMovesCount(int movesCount)
+	{
+		this.movesCount = movesCount;
+		countText.text = movesCount.ToString();
+	}
+
+}
